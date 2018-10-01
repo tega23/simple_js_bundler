c10580df4e6194c3bd5d93de686e02cd259ccb3f
Tega Erekata <tega23@yahoo.com>
2018-10-01 15:25:24 +0100

    chore : created dependeny graph for files

---
+++ bundler.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const path = require('path');
+const babylon = require('babylon');
+const traverse = require('babel-traverse').default;
+
+let ID = 0;
+function createAssets(filename) {
+  console.log(filename);
+  const content = fs.readFileSync(filename, 'utf-8');
+
+  const ast = babylon.parse(content, {
+    sourceType: 'module'
+  });
+
+  const dependencies = [];
+
+  traverse(ast, {
+    ImportDeclaration: ({ node }) => {
+      dependencies.push(node.source.value);
+    }
+  });
+
+  const id = ID++;
+
+  return {
+    id,
+    filename,
+    dependencies
+  };
+}
+
+function createGraph(entry) {
+  const mainAsset = createAssets(entry);
+
+  const queue = [mainAsset];
+
+  for (const asset of queue) {
+    const dirname = path.dirname(asset.filename);
+    asset.mapping = {};
+
+    asset.dependencies.forEach(relativePath => {
+      const absolutePath = path.join(dirname, relativePath);
+      const child = createAssets(absolutePath);
+
+      asset.mapping[relativePath] = child.id;
+
+      queue.push(child);
+    });
+  }
+
+  return queue;
+}
+
+const graph = createGraph('./entry.js');
+
+console.log(graph);
